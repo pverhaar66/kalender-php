72d102dd502ccb7a382fb1576195e5e82e9a6e04
Peter Verhaar <99021508@mydavinci.nl>
2017-03-21 11:03:31 +0100

    kalender done [Khorosho]

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php 
+	$db = new PDO("mysql:host=localhost;dbname=calendar", "root", "");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+
+	$sql = "DELETE FROM birthdays WHERE id=:id";
+	// :id tells the Database that there will be an ID input later
+	$stmt = $db->prepare($sql);
+	$stmt->execute(array(":id"=>$_GET['id'])); 
+	// sends the inputtted ID to the Database in a array
+
+?>
+
+
+<?php  require('header.php');	 ?>
+<body>
+	<main>
+		<section>
+			<h1> Delete succes :D (all deleted people will die for real ! )</h1>
+			<a href="index.php"><button>Back</button></a>
+		</section>
+	</main>
+</body>
